8d6c5124d1610ee1712db5273e62b217bfae02e3
Raf Van Baelen <rafvanbaelen@gmail.com>
2020-12-09 21:39:43 +0100

    Add project files.

---
+++ Raf.YNAB.Importer/Parsers.fs
@@ -0,0 +1,26 @@
+﻿namespace Raf.YNAB.Importer
+
+open System.Globalization
+open FSharp.Data
+
+type Transactions = CsvProvider<"sample.csv", EmbeddedResource="Raf.YNAB.Importer, Raf.YNAB.Importer.sample.csv", Separators=";">
+type YNABTransactions = CsvProvider<"ynab.csv", Schema="Date (string), Payee (string), Memo (string), Outflow (string), Inflow (string)",
+                                                EmbeddedResource="Raf.YNAB.Importer, Raf.YNAB.Importer.ynab.csv">
+
+module FortisParser =
+    let parse file =
+        let transactions = Transactions.Parse(file)
+
+        let ynabTransactions =  
+            transactions.Rows             
+            |> Seq.map (fun row -> 
+                let formattedBedrag = (abs row.Bedrag).ToString("F", CultureInfo.InvariantCulture)
+
+                let outflow = if row.Bedrag < 0m then formattedBedrag else ""
+                let inflow = if row.Bedrag > 0m then formattedBedrag else ""
+
+                YNABTransactions.Row(row.Uitvoeringsdatum, row.Details, row.Details, outflow, inflow))
+            |> Seq.toList
+
+        let ynab = new YNABTransactions(ynabTransactions)
+        ynab.SaveToString()
